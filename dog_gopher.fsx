d13f24d3fe58bc355a3886c0dc09871b66e471b3
shane charles <shanecharles@burningicesolutions.com>
2016-09-15 13:09:18 -0500

    improve readability of the parseInput function.

--- dog_gopher.fsx
+++ dog_gopher.fsx
@@ -3,14 +3,14 @@ type Point = float * float
 type InputSet = { gopher : Point; dog : Point; holes : Point list }
 
 let parseInput file =
-  let newSet [| _; x1; y1; x2; y2 |] = {gopher = (x1, y1); dog = (x2, y2); holes = []}
   file |> System.IO.File.ReadAllLines |> Seq.filter (fun l -> not <| System.String.IsNullOrEmpty l )
-  |> Seq.fold (fun sets l -> l.Split(' ') |> Array.map float
-                             |> function [| x; y |] -> let h::t = sets
-                                                       {h with holes = ((x, y) :: h.holes)} :: t
-                                       | pts -> (newSet pts) :: sets) []
-  |> Seq.rev
-  |> Seq.map (fun p -> {p with holes = p.holes |> List.rev})
+  |> Seq.map (fun l -> l.Split(' ') |> Array.map float)
+  |> Seq.fold (fun sets ls -> match sets, ls with 
+                              | _, [| _; x1; y1; x2; y2 |] -> {gopher = (x1, y1); dog = (x2, y2); holes = []} :: sets
+                              | (h::t, [| x; y|])          -> {h with holes = (x,y) :: h.holes} :: t
+                              | _                          -> failwith "invalid input") []
+  |> Seq.rev 
+  |> Seq.map (fun p -> {p with holes = (List.rev p.holes)})
 
 let distance (x1, y1) (x2, y2) =
   let difference x' y' = (x' - y') * (x' - y')
@@ -24,5 +24,5 @@ let findSafeHole data =
 let printResult = function Some p -> printfn "The gopher can escape through the hole at %A" p
                          | _      -> printfn "The gopher cannot escape."
 
-let simulate = parseInput >> Seq.map findSafeHole >> Seq.iter printResult
+let simulate : string -> unit = parseInput >> Seq.map findSafeHole >> Seq.iter printResult
 simulate input
